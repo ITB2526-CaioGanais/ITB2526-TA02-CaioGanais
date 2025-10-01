a330fd68bca7f972bc54a1fe84e54a63a79e88db
caio.ganais.7e9 <caio.ganais.7e9@itb.cat>
2025-10-01 09:54:11 +0200

    comproveAge Update

--- TA02/ComprovarEdat.py
+++ TA02/ComprovarEdat.py
@@ -1,9 +1,21 @@
+from datetime import date
+from datetime import datetime
+
+
 def comproveAge():
-    age=int(input("Quina edat tens? "))
-    year= 2025-age;
+    today = date.today()
+    print("Fecha actual:", today)
+
+    birth = input("¿En qué año naciste? (Introduzca la fecha en forma YYYY-MM-DD): ")
+    year, month, day = map(int, birth.split('-'))
+    birth_date = date(year, month, day)
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    print("Edad:", age, "años")
+
     if age >= 18:
-        print("Ets major d'edat i has nascut al",year)
-        print("Programa Finalitzat")
+        print("Ets major d'edat")
     else:
-        print("Ets menor d'edat i has nascut al", year)
-        print("Programa Finalitzat")
+        print("Ets menor d'edat")
+
+
+
